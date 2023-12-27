9be42f4e83c3bd8667ecfa6270c5330d6e171ae3
Max Greenwald <powerslab3@gmail.com>
2023-12-27 16:01:38 -0500

    copied over files from achshortd2 changed package.json and config.json names and npm install

---
+++ build/precache-manifest.62a17620bb97521d35e9f22cfab9eddb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d022abd66d11442cadd4ba4c328ca4de",
+    "url": "/achshortd2/index.html"
+  },
+  {
+    "revision": "e4cfdd9218af75a5f041",
+    "url": "/achshortd2/static/css/main.f4ac948c.chunk.css"
+  },
+  {
+    "revision": "343991a5f4da0545203c",
+    "url": "/achshortd2/static/js/2.4252abb9.chunk.js"
+  },
+  {
+    "revision": "ee45c4ac1e2cdbe20661beb901b1137f",
+    "url": "/achshortd2/static/js/2.4252abb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4cfdd9218af75a5f041",
+    "url": "/achshortd2/static/js/main.8d9ce436.chunk.js"
+  },
+  {
+    "revision": "e78ceaabf68a6f283046",
+    "url": "/achshortd2/static/js/runtime-main.e03eee3a.js"
+  },
+  {
+    "revision": "6956dbda0015b26b8daac462ec922ef5",
+    "url": "/achshortd2/static/media/psych_logo.6956dbda.jpg"
+  },
+  {
+    "revision": "d9fbf22f557b87c921a73ef996721520",
+    "url": "/achshortd2/static/media/rating_keydown_1.d9fbf22f.png"
+  },
+  {
+    "revision": "6a659dd41fa6c15739f611662bf6d8d1",
+    "url": "/achshortd2/static/media/rating_keydown_2.6a659dd4.png"
+  },
+  {
+    "revision": "d955b91a2b4ba7dca40ee5ccb82d995b",
+    "url": "/achshortd2/static/media/rating_keydown_3.d955b91a.png"
+  },
+  {
+    "revision": "c8b7c6860406752a1915e08a432cb320",
+    "url": "/achshortd2/static/media/rating_keydown_4.c8b7c686.png"
+  },
+  {
+    "revision": "34958048ce563b1140c19e19f6e72410",
+    "url": "/achshortd2/static/media/rating_keydown_5.34958048.png"
+  }
+]);
